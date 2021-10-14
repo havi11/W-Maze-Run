bcfec99bcefec5e858608944d518dc10561ad34f
havi11 <eddievillegas0510@hotmail.com>
2021-10-13 23:27:07 -0400

    Add death mode 1.0

--- src/client/MovingStopped.client.lua
+++ src/client/MovingStopped.client.lua
@@ -6,6 +6,8 @@ local BlockPart = Instance.new("Part", Repstore)
 BlockPart.Size = Vector3.new(0,0,0)
 BlockPart.Anchored = true
 local UIS =game:GetService("UserInputService")
+local Values = player:WaitForChild("Values")
+local SS = game.SoundService
 local TS = game:GetService("TweenService")
 local Tween = {}
 Tween.Size = Size1
@@ -20,10 +22,31 @@ end)]]--
 wait(5)
 player.Character.Humanoid.Running:Connect(function(speed)
     if speed == 0 then
-        local Copy = BlockPart:Clone()
-        Copy.Parent = game.Workspace
-        Copy.Position = player.Character.LowerTorso.Position +Position1
-        local T = TS:Create(Copy, Settings, Tween)
-        T:Play()
+        if Values.CurMode.Value == 2 then
+            print("Died")
+            player.Character.Humanoid.Health = 1
+            player.Character:MoveTo(game.Workspace.SpawnLocation.Position)
+            local DeathScreen = Instance.new("ScreenGui", player.PlayerGui)
+            DeathScreen.Name = "DeathGui"
+            DeathScreen.IgnoreGuiInset = true
+            local DFrame = Instance.new("Frame", DeathScreen)
+            DFrame.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+            DFrame.Size = UDim2.new(1,0,1,0)
+            if Values.CurMaze.Value == 1 then
+                SS.Maze0:Stop()
+                SS.DeathStart:Play()
+            elseif Values.CurMaze.Value == 2 then
+                SS.Maze1:Stop()
+                SS.DeathStart:Play()
+            end
+            SS.DeathStart:Play()
+        elseif Values.CurMode.Value == 1 then
+            local Copy = BlockPart:Clone()
+            Copy.Parent = game.Workspace
+            Copy.Position = player.Character.LowerTorso.Position +Position1
+            local T = TS:Create(Copy, Settings, Tween)
+            T:Play()
+        end
+
     end
 end)
